8b4bc29938a2cc74d9d1e68099b5995764a82035
Annabelle Huang <annabellehuang@wirelessprv-10-192-96-48.near.illinois.edu>
2017-07-23 17:40:59 -0500

    July 23rd modification

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'racialjustice'
+    'csvimport.app.CSVImportConf',
+    'racialjustice',
+    
 )
 
 MIDDLEWARE_CLASSES = (
--- racialjustice/models.py
+++ racialjustice/models.py
@@ -3,3 +3,27 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Arrest(models.Model):
+    arrestee_id = models.IntegerField()
+    arrest_date = models.DateField()
+    arrest_time = models.TimeField()
+    arrest_code = models.TextField()
+    arrest_location = models.TextField()
+    crime_code = models.TextField()
+    crime_code_description = models.TextField()
+    crime_code_category = models.TextField()
+    crime_code_category_description = models.TextField()
+    weapon_code_1 = models.CharField(max_length=30)
+    weapon_code_2 = models.CharField(max_length=30)
+    weapon_code_3 = models.CharField(max_length=30)
+    race = models.TextField()
+    sex = models.TextField()
+    age = models.IntegerField()
+    employment_code = models.CharField(max_length=30)
+    arrest_officer_code = models.IntegerField()
+    result = models.TextField()
+    jurisdiction = models.TextField()
+    more_than_one_charge = models.IntegerField()
+    mvo = models.IntegerField()
+    
