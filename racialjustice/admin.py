8746aa5dbc932be1c3ccab2065c23c5911354aec
Annabelle Huang <annabellehuang@wirelessprv-10-192-96-48.near.illinois.edu>
2017-08-05 17:08:36 -0500

    gitignore change

--- racialjustice/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Arrest
-admin.site.register(Arrest)
-from .models import Greeting
-admin.site.register(Greeting)
